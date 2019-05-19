1cff01dcecb2fe30780f54b00213ce11647793d5
Brennii96 <brendan.oneill96@hotmail.co.uk>
2019-05-19 22:37:11 +0100

    Initial Commit

---
+++ admin/partials/typed-js-admin-display.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * Provide a admin area view for the plugin
+ *
+ * This file is used to markup the admin-facing aspects of the plugin.
+ *
+ * @link       https://github.com/Brennii96/
+ * @since      1.0.0
+ *
+ * @package    Typed_Js
+ * @subpackage Typed_Js/admin/partials
+ */
+?>
+
+<!-- This file should primarily consist of HTML with a little bit of PHP. -->
+
+<div class="wrap">
+    <h1>Typed JS</h1>
+
+    <p>To use this plugin simply enter your content between the shortcode tags <strong>[typedjs]<em>content</em>[/typedjs]</strong>.</p>
+
+    <p>To use the backspace feature add a plus sign '+'.</p>
+
+    <p>It will loop continuously.</p>
+</div>
---
+++ typed-js.php
@@ -0,0 +1,82 @@
+<?php
+
+/**
+ * The plugin bootstrap file
+ *
+ * This file is read by WordPress to generate the plugin information in the plugin
+ * admin area. This file also includes all of the dependencies used by the plugin,
+ * registers the activation and deactivation functions, and defines a function
+ * that starts the plugin.
+ *
+ * @link              https://github.com/Brennii96/
+ * @since             1.0.0
+ * @package           Typed_Js
+ *
+ * @wordpress-plugin
+ * Plugin Name:       TypedJs
+ * Plugin URI:        https://github.com/Brennii96/TypedJs
+ * Description:       This is a small plugin which allows you to use Typed JS when you put text between the shortcode [typedjs]Content[/typedjs]
+ * Version:           1.0.0
+ * Author:            Brendan O'Neill
+ * Author URI:        https://github.com/Brennii96/
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain:       typed-js
+ * Domain Path:       /languages
+ */
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
+/**
+ * Currently plugin version.
+ * Start at version 1.0.0 and use SemVer - https://semver.org
+ * Rename this for your plugin and update it as you release new versions.
+ */
+define( 'TYPED_JS_VERSION', '1.0.0' );
+
+/**
+ * The code that runs during plugin activation.
+ * This action is documented in includes/class-typed-js-activator.php
+ */
+function activate_typed_js() {
+	require_once plugin_dir_path( __FILE__ ) . 'includes/class-typed-js-activator.php';
+	Typed_Js_Activator::activate();
+}
+
+/**
+ * The code that runs during plugin deactivation.
+ * This action is documented in includes/class-typed-js-deactivator.php
+ */
+function deactivate_typed_js() {
+	require_once plugin_dir_path( __FILE__ ) . 'includes/class-typed-js-deactivator.php';
+	Typed_Js_Deactivator::deactivate();
+}
+
+register_activation_hook( __FILE__, 'activate_typed_js' );
+register_deactivation_hook( __FILE__, 'deactivate_typed_js' );
+
+/**
+ * The core plugin class that is used to define internationalization,
+ * admin-specific hooks, and public-facing site hooks.
+ */
+require plugin_dir_path( __FILE__ ) . 'includes/class-typed-js.php';
+
+/**
+ * Begins execution of the plugin.
+ *
+ * Since everything within the plugin is registered via hooks,
+ * then kicking off the plugin from this point in the file does
+ * not affect the page life cycle.
+ *
+ * @since    1.0.0
+ */
+function run_typed_js() {
+
+	$plugin = new Typed_Js();
+	$plugin->run();
+
+}
+run_typed_js();
